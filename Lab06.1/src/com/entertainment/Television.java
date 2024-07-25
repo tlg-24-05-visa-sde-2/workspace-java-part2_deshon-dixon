29b98e7199e236c979bd7405519355f6e381ce51
DeShon Dixon <deshondixon@gmail.com>
2024-07-25 16:01:49 -0500

    Tuner as true inner class, DisplayType as static nested classes

--- Lab06.1/src/com/entertainment/Television.java
+++ Lab06.1/src/com/entertainment/Television.java
@@ -8,6 +8,7 @@
  */
 package com.entertainment;
 
+import java.util.Comparator;
 import java.util.Objects;
 
 public class Television implements Comparable<Television> {
@@ -113,5 +114,44 @@ public class Television implements Comparable<Television> {
                      Objects.equals(this.getDisplay(), other.getDisplay());
         }
         return result;
+    }
+
+    private static class Tuner {
+        private int channel = 3;
+
+        public int getChannel() {
+            return this.channel;
+        }
+
+        public void setChannel(int channel) {
+            this.channel = channel;
+        }
+    }
+
+    // STATIC NESTED CLASS
+    // outside here, this is referred to as Television.DisplayType
+    public static enum DisplayType {
+        LCD, LED, OLED, PLASMA, CRT
+    }
+
+    public static class ChannelComparator implements Comparator<Television> {
+
+        @Override
+        public int compare(Television tv1, Television tv2) {
+            return Integer.compare(tv1.getCurrentChannel(), tv2.getCurrentChannel());
+        }
+    }
+
+    public static class BrandChannelComparator implements Comparator<Television> {
+
+        @Override
+        public int compare(Television tv1, Television tv2) {
+            int result = tv1.getBrand().compareTo(tv2.getBrand());
+
+            if (result == 0) {
+                result = Integer.compare(tv1.getCurrentChannel(), tv2.getCurrentChannel());
+            }
+            return result;
+        }
     }
 }
