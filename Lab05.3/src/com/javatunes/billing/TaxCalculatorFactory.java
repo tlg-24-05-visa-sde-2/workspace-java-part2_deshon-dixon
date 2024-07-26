1eb3aa5c745e596e60994eadd2f7e27ce594f062
DeShon Dixon <deshondixon@gmail.com>
2024-07-26 09:29:13 -0500

    Enhanced switch expression

--- Lab05.3/src/com/javatunes/billing/TaxCalculatorFactory.java
+++ Lab05.3/src/com/javatunes/billing/TaxCalculatorFactory.java
@@ -2,19 +2,16 @@ package com.javatunes.billing;
 
 public class TaxCalculatorFactory {
 
+    private TaxCalculatorFactory() {
+
+    }
+
     // no instantiation from outside, this is an all-static class
     public static TaxCalculator createTaxCalculator(Location location) {
-        TaxCalculator calc = null;
-        switch (location) {
-            case ONLINE:
-                calc = new OnlineTax();
-                break;
-            case USA:
-                calc = new USATax();
-                break;
-            case EUROPE:
-                new EuropeTax();
-                break;
+        TaxCalculator calc = switch (location) {
+            case ONLINE -> new OnlineTax();
+            case USA -> new USATax();
+            case EUROPE -> new EuropeTax();
         };
         return calc;
     }
